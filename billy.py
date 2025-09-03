7c804fad30cf4e371ffb253781c4a7a6ad87f0aa
PinePlayz <88686590+PineappleGoob@users.noreply.github.com>
2025-09-02 20:41:45 -0400

    Update billy.py

--- billy.py
+++ billy.py
@@ -6,23 +6,33 @@ from getpass import getpass
 from argparse import ArgumentParser
 import queue
 import discord
+from discord import app_commands, SyncWebhook
 import threading
 from threading import Lock
 import xmpp
 import slixmpp_omemo
 import time
 import aiohttp
+from discord.ext import commands
+import json
 import base64
+import requests
 #discord setup
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
+tree = app_commands.CommandTree(client)
 # event from xmpp to discord
 tevent = asyncio.Event()
+xmpp_loop = None
+
 #event from discord to xmpp
 devent = threading.Event()
 
+with open('config.json', 'r') as file:
+    config = json.load(file)
+with open('webhook.json', 'r') as file:
+    webhooks = json.load(file)
 
 avatar_data = None
 #list for xmpp messages to queue up for discord to send
@@ -31,18 +41,78 @@ list_lock = Lock()
 
 #list for discord messages to queue up for xmpp to send
 dshared_list = []
-
+xqueue = asyncio.Queue()
 
 #discord on startup
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await messagemachine()
+    await tree.sync()
+    asyncio.create_task(messagemachine())
 			#send message 
+
+webhookurl = None
+@tree.command(name='setup', description='Run this before you do anything. Very important')
+async def setup(interaction: discord.Interaction):
+    channel = interaction.channel
+    global webhookurl
+    for mapping in webhooks.get("guildids", []):
+        print(interaction.guild_id)
+        print(mapping)
+        if str(interaction.guild_id) in mapping:
+            webhookurl = mapping[str(interaction.guild_id)]
+            print(webhookurl)
+            break
+    if webhookurl:
+        webhooksending = SyncWebhook.from_url(webhookurl)
+        webhooksending.send('test')
+    else:
+        webhoook = await channel.create_webhook(name='Sky.Bit')
+        webhooks["guildids"].append({str(interaction.guild_id): str(webhoook.url)})
+        with open('webhook.json', 'w') as file:
+            json.dump(webhooks, file, indent=4)
+
+
+
+
+#manages sending the queued xmpp messages to da discord
+import aiohttp
+from discord import Webhook
+
+async def messagemachine():
+    global xshared_list, webhookurl
+    channel = client.get_channel(1065988089769631796)
+
+    async with aiohttp.ClientSession() as session:
+        while True:
+            if xshared_list:
+                messagetosend = xshared_list.pop(0)
+
+                if webhookurl:
+                    webhook = Webhook.from_url(webhookurl, session=session)
+                    await webhook.send(
+                        messagetosend,
+                        username='SkydotBit',
+                        avatar_url='https://skydevs.me/foxxo.gif'
+                    )
+                    print('Message sent via async webhook')
+                else:
+                    await channel.send('Run /setup to make stuff work')
+
+            await asyncio.sleep(0.05)  # yield control to the event loop
+
 
 #discord to xmpp
 @client.event
 async def on_message(message):
+    if message.author.bot or message.author == client.user:
+        return
+    
+    print(message.content)
+    # Push the message to the XMPP queue
+    dshared_list.append(message.content)
+    print("Message queued for XMPP")
+"""""
     #if message is from bot return
     if message.author == client.user:
         return
@@ -54,44 +124,29 @@ async def on_message(message):
         #puts message in da queue
         dshared_list.append(message.content)
         print(dshared_list)
-        jid = xmpp.protocol.JID('dougdoug@xmpp.skydevs.me')
+        jid = xmpp.protocol.JID(config['User'])
         connection = xmpp.Client(server=jid.getDomain(), debug=True)
         connection.connect()
-        msgs = dshared_list.pop(0)
-        connection.auth(user=jid.getNode(), password='dougdoug', resource=jid.getResource())
-        connection.send(xmpp.protocol.Message(to='jibberbob@conversations.im', body=msgs))
+        connection.auth(user=jid.getNode(), password=config['Pass'], resource=jid.getResource())
+        connection.sendInitPresence()  # essential
+        while dshared_list:
+            msgs = dshared_list.pop(0)
+            connection.send(xmpp.protocol.Message(to=config['Recipient'], body=msgs, typ="chat"))
+
     #devent.set() temporaryily disable until find better fix here.
     print('event set')
-
-#manages sending the queued xmpp messages to da discord
-async def messagemachine():
-        while True:
-            print('waiting for event')
-            await tevent.wait()
-            print('evento activato')
-            channel = client.get_channel(1065988089769631796)
-            global xshared_list
-
-            messagetosend = xshared_list.pop(0)
-            tevent.clear()
-            async with aiohttp.ClientSession() as session:
-                async with session.get("https://skydevs.me/foxxo.gif") as resp:
-                    avatar_bytes = await resp.read()
-
-            webhoook = await channel.create_webhook(name='Sky.Bit', avatar=avatar_bytes)
-            await webhoook.send(messagetosend)	
-            print('sent')
-
-
-
+"""
 #xmpp class
 class BOot(ClientXMPP):
     #initalization
     def __init__(self, jid, password):
         super().__init__(jid, password) 
-        
+   
+        self.room = 'xmpp:hasibixo@muc.xmpp.skydevs.me'
+        self.nick = 'Discord'     
         self.add_event_handler('session_start', self.start)
         self.add_event_handler('message', lambda msg: asyncio.create_task(self.message(msg)))
+
         #self.register_plugin('xep_0384')
 
 
@@ -101,7 +156,8 @@ class BOot(ClientXMPP):
         print('started and connected to xmpp')
         self.send_presence()
         await self.get_roster()
-
+        #self.plugin['xep_0045'] 
+        asyncio.create_task(self.process_discord_queue())
     async def messagemachine(self):
         
 
@@ -127,7 +183,7 @@ class BOot(ClientXMPP):
 
 
     async def retrieve_avatar(self, jid):
-        vcard = await self['xep_0054'].get_vcard(jid='sky.bit@xmpp.skydevs.me')
+        vcard = await self['xep_0054'].get_vcard(jid=config['Recipient'])
 
         photo_elem = vcard.xml.find('{vcard-temp}PHOTO')  # correct namespace
         if photo_elem is not None:
@@ -140,7 +196,20 @@ class BOot(ClientXMPP):
         else:
             print("No photo element found")
 
-
+    async def process_discord_queue(self):
+        while True:
+            global dshared_list
+            if dshared_list:
+                with list_lock:  # lock to prevent race conditions
+                    msg = dshared_list.pop(0)
+                print('sending')
+                self.send_message(
+                    mto=str(config['Recipient']),
+                    mbody=msg,
+                    mtype='chat'
+                )
+                print(f"Sent to XMPP: {msg}")
+            await asyncio.sleep(0.05)  # yield to the event loop
 
 
     #on message
@@ -149,7 +218,7 @@ class BOot(ClientXMPP):
         #prints message
         print(msg)
         print(msg['body'])
-        await self.retrieve_avatar('sky.bit@xmpp.skydevs.me')
+        await self.retrieve_avatar(config['Recipient'])
 
         #decrypted = self['xep_0384'].decrypt_message(msg['body'])
         #print(decrypted)
@@ -157,10 +226,10 @@ class BOot(ClientXMPP):
         with list_lock:
             #puts message in da queue
             xshared_list.append(msg['body'])
+            tevent.set()
             print(xshared_list)
         print()
         #sets off tevent
-        tevent.set()
         if msg['type'] in ('normal', 'chat'):
             #if encrypted request decryption
             if msg['body'] == '[This message is OMEMO encrypted]':
@@ -179,9 +248,10 @@ class BOot(ClientXMPP):
 def xmppthing(): 
 	# 1. Create a new event loop for this thread.
 	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(xmpp_loop)
 	# 2. Set the new event loop as the current one for this thread.
 	asyncio.set_event_loop(loop)
-	xmpp = BOot ('dougdoug@xmpp.skydevs.me', 'dougdoug') #jid, password
+	xmpp = BOot (config['User'], config['Pass']) #jid, password
 	xmpp.register_plugin('xep_0030') # Service Discovery
 	xmpp.register_plugin('xep_0199') # Ping
 	xmpp.register_plugin('xep_0084') # omemo :)
@@ -191,13 +261,14 @@ def xmppthing():
 	xmpp.connect()
 	# 3. Use the correct loop object to run the Slixmpp processing loop.
 	loop.run_forever()
+	loop.call_soon_threadsafe(tevent.set)
 
 if __name__ == '__main__':
 
         
      #discord starting   
     def discordthing(): 
-        client.run('MTQxMDA0MTE2NzQ5MTM2NzEzMg.G_I9SK.s8GgS7TUX2Iy7KUPaG9dnNasAyqowqe7i3vYN8')
+        client.run(config['token'])
         
      #manages all the threads and shit   
     thread1 = threading.Thread(target=discordthing)
